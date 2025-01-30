924d5a5044a98135fa7e41f6370ea7d95d8cafad
one4all <seodj93@kakao.com>
2025-01-30 17:51:29 +0900

    intro추가

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="intro component">
-    <h1>내가 개발자가 되고</h1>
+    <video src="../mp4/oneForAll.mp4" width="100%" autoplay muted loop></video>
+
+    <!-- <img src="../img/intro.png" width="100%" alt="intro" /> -->
+
+    <h1>이 이야기는</h1>
+
+    <h1>내가 개발자가 되기까지</h1>
+
+    <h1>그리고</h1>
+
     <h1>개발자로서 살아가는 이야기다!!</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Intro',
+}
+</script>
+
+<style></style>
